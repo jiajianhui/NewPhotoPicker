69fe07d87128310de97cea091c5c2417141458b9
jianhui <78072865+jiajianhui@users.noreply.github.com>
2025-07-08 11:08:20 +0800

    Initial Commit

---
+++ NewPhotoPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewPhotoPicker
+//
+//  Created by 贾建辉 on 2025/7/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewPhotoPicker/NewPhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewPhotoPickerApp.swift
+//  NewPhotoPicker
+//
+//  Created by 贾建辉 on 2025/7/8.
+//
+
+import SwiftUI
+
+@main
+struct NewPhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
